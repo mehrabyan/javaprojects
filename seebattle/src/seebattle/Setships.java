104a99eab65d575f0287794eed69306d525e0305
Armen Mehrabyan <armenrt@yahoo.com>
2016-09-05 12:29:29 +0400

    I forgot add Setships.java file

--- seebattle/src/seebattle/Setships.java
+++ seebattle/src/seebattle/Setships.java
@@ -1,19 +1,16 @@
 package seebattle;
 
 public class Setships {
-	Coordinate c = new Coordinate();
 	Ship ship;
 	Shipsfield shipsfield;
 
 	public void setShip4ForTest(Shipsfield shipsfield) {
 // set 4
 		ship = new Ship();
-		c.setC(1, 0);
-		for (int i = 1;i < 5; i++) {
-			c = c.nextInColm();
-			ship.addToBlocks(c);
-			shipsfield.removeFromEmptyFields(c);
-		}
+		ship.addToBlocks(new Coordinate(1, 1));
+		ship.addToBlocks(new Coordinate(1, 2));
+		ship.addToBlocks(new Coordinate(1, 3));
+		ship.addToBlocks(new Coordinate(1, 4));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 	}
 		
@@ -21,27 +18,15 @@ public class Setships {
 // I - 3		
 	public void setShip3ForTest(Shipsfield shipsfield) {
 		ship = new Ship();
-		c = new Coordinate(7, 6);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(7, 7);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(7, 8);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(7, 6));
+		ship.addToBlocks(new Coordinate(7, 7));
+		ship.addToBlocks(new Coordinate(7, 8));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 // II - 3
 		ship = new Ship();
-		c = new Coordinate(3, 6);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(4, 6);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(5, 6);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(3, 6));
+		ship.addToBlocks(new Coordinate(4, 6));
+		ship.addToBlocks(new Coordinate(5, 6));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		}
 //	
@@ -50,59 +35,39 @@ public class Setships {
 		
 	public void setShip2ForTest(Shipsfield shipsfield) {
 		ship = new Ship();
-		c = new Coordinate(3, 2);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(3, 3);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(3, 2));
+		ship.addToBlocks(new Coordinate(3, 3));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		
 // II - 2
 		ship = new Ship();
-		c = new Coordinate(6, 1);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(6, 2);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(6, 1));
+		ship.addToBlocks(new Coordinate(6, 2));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		
 // III - 2
 		ship = new Ship();
-		c = new Coordinate(2, 8);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
-		c = new Coordinate(3, 8);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(2, 8));
+		ship.addToBlocks(new Coordinate(3, 8));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 	}	
 // set 1
 	
 	public void setShip1ForTest(Shipsfield shipsfield) {
 		ship = new Ship();
-		c = new Coordinate(8, 1);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(8, 1));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		
 		ship = new Ship();
-		c = new Coordinate(5, 4);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(5, 4));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		
 		ship = new Ship();
-		c = new Coordinate(7, 4);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(7, 4));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
 		
 		ship = new Ship();
-		c = new Coordinate(5, 8);
-		ship.addToBlocks(c);
-		shipsfield.removeFromEmptyFields(c);
+		ship.addToBlocks(new Coordinate(5, 8));
 		shipsfield.addToListShips(new Ship(ship.getBlocks()));
    }
 }
