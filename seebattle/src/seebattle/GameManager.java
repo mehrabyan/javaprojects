9ca8ba3457eb8bf34d5b05a9ccc9d43168bbc87f
Darth Spectrum <muradianator@gmail.com>
2016-09-10 16:38:47 +0400

    Forgot to add modified classes for previous commit

--- seebattle/src/seebattle/GameManager.java
+++ seebattle/src/seebattle/GameManager.java
@@ -4,35 +4,50 @@ package seebattle;
 
 public class GameManager {
 	Setships set = new Setships();
-	Player player1,player2;
-	Shipsfield shipsfield1,shipsfield2;
-	int ind = 0;
-	GameManager () {
-		player1 = new Player();
-		player2 = new Player();
-	    shipsfield1 = new Shipsfield();
-	    shipsfield2 = new Shipsfield();
-		} //konec konstruktora
-	private void createEmptyFields () {
+	Player player1, player2, currentPlayer;
+	Shipsfield shipsfield1, shipsfield2, currentShipsfield;
+
+	GameManager() {
+		player1 = new Player("Player1");
+		player2 = new Player("Player2");
+		shipsfield1 = new Shipsfield();
+		shipsfield2 = new Shipsfield();
+		currentPlayer = player1;
+		currentShipsfield = shipsfield2;
+	} // konec konstruktora
+
+	private void switchPlayer() {
+		if (currentPlayer == player1) {
+			currentPlayer = player2;
+			currentShipsfield = shipsfield1;
+		} else {
+			currentPlayer = player1;
+			currentShipsfield = shipsfield2;
+		}
+	}
+
+	private void createEmptyFields() {
 		shipsfield1.fillEmptyFields();
 		System.out.println("The size of emptyFields after creating shipsfield1 is :" + shipsfield1.sizeOfEmptyFields());
 		shipsfield2.fillEmptyFields();
 		System.out.println("The size of emptyFields after creating shipsfield2 is :" + shipsfield1.sizeOfEmptyFields());
-	}	
-	
+	}
+
 	private void setShips() {
 		set.setShip4ForTest(shipsfield1);
 		set.setShip3ForTest(shipsfield1);
 		set.setShip2ForTest(shipsfield1);
 		set.setShip1ForTest(shipsfield1);
-		System.out.println("The size of emptyFields after ships seting on the shipsfield1 is :" + shipsfield1.sizeOfEmptyFields());
+		System.out.println(
+				"The size of emptyFields after ships seting on the shipsfield1 is :" + shipsfield1.sizeOfEmptyFields());
 		System.out.println("The blocks :" + shipsfield1.toString());
-		
+
 		set.setShip4ForTest(shipsfield2);
 		set.setShip3ForTest(shipsfield2);
 		set.setShip2ForTest(shipsfield2);
 		set.setShip1ForTest(shipsfield2);
-		System.out.println("The size of emptyFields after ships seting on the shipsfield2 is :" + shipsfield2.sizeOfEmptyFields());
+		System.out.println(
+				"The size of emptyFields after ships seting on the shipsfield2 is :" + shipsfield2.sizeOfEmptyFields());
 		System.out.println("The blocks :" + shipsfield2.toString());
 	}
 
@@ -40,91 +55,50 @@ public class GameManager {
 		createEmptyFields();
 		setShips();
 	}
-	
-//	make Game Manager really GM // all functions to flow game
-	private ShotResult pliPlayer(Player player,Shipsfield shipsfield) {
+
+	// make Game Manager really GM // all functions to flow game
+	private ShotResult pliPlayer() {
 		Coordinate c;
-		ShotResult res;
-		c = player.pli();
+		c = currentPlayer.pli();
 		System.out.println("The target is :" + c.toString());
-		return res = shipsfield.checkShot(c);
+		return currentShipsfield.checkShot(c);
 	}
-	
-	public void switchPlayer1() {
-		System.out.print("Plauer1 is shooting.");
-		switch (pliPlayer(player1,shipsfield2)) {
-			
-		case HIT:
-			System.out.println("The ship is hit, Player1 continues shooting");
-			switchPlayer1();
-			break;
-		case DEAD:
-			System.out.println("The ship is dead");
-			if (!(shipsfield2.checkListOfShips() == 0)) {
-				System.out.println("Continues shooting");
-			  	switchPlayer1();
-			  	break;
-			  	}
-			else 
-			  System.out.println("Player1 wins.Game over");
-			  System.out.println();
-				ind = 1;
-//			  Gameover.functions;
-//			  Show all statistic dates about shots,dead and hit ships
-				break;
-		  	
-		case MISS:
-			System.out.println("The shot miss,switch Player");
-			switchPlayer2();
-			break;
-		default:
-			break;
-	   }
-	}	
-	
-	public void switchPlayer2() {
-		System.out.print("Plauer2 is shooting.");
-		switch (pliPlayer(player2,shipsfield1)) {
-			
-		case HIT:
-			System.out.println("The ship is hit, Player2 continues shooting");
-			switchPlayer2();
-			break;
-		case DEAD:
-			System.out.println("The ship is dead");
-			if (!(shipsfield1.checkListOfShips() == 0)) {
-				System.out.println("Continues shooting");
-			  	switchPlayer2();
-			  	break;
-			  	}
-			else
-			  System.out.println("Player2 wins.Game over");
-			  System.out.println();
-				ind = 1;
-//			  Gameover.functions;
-//			  Show all statistic dates about shots,dead and hit ships
-			
-			  break;
-		  	
-		case MISS:
-			System.out.println("The shot miss,switch Player");
-			switchPlayer1();
-			break;
-		default:
-			break;
-	   }
-	}	
+
 	public void naabordaj() {
-		do {
-		switchPlayer1();
-		} while (!(ind == 1));
-	}		
-	
+		boolean gameOver = false;
+		while (!gameOver) {
+			 System.out.print(currentPlayer.getName() + " is shooting.");
+			switch (pliPlayer()) {
+
+			case HIT:
+				System.out.println("The ship is hit, " + currentPlayer.getName() + " continues shooting");
+				break;
+			case DEAD:
+				System.out.println("The ship is dead");
+				if (!(currentShipsfield.checkListOfShips() == 0)) {
+					System.out.println("Continues shooting");
+				} else {
+					gameOver = true;
+					 System.out.println(currentPlayer.getName() + " wins.Game over");
+					System.out.println();
+				}
+				break;
+
+			case MISS:
+				System.out.println("The shot miss,switch Player");
+				switchPlayer();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
 	public void statistics() {
 		Bookkeeping book = new Bookkeeping();
 		book.allDeadShips(shipsfield1, shipsfield2);
 		book.showShots(player1, player2);
 		book.showHitShips(shipsfield1, shipsfield2);
-		
+
 	}
 }
--- seebattle/src/seebattle/Player.java
+++ seebattle/src/seebattle/Player.java
@@ -1,41 +1,53 @@
 // Sozdaem class igrokov
 package seebattle;
+
 import java.util.*;
 
 public class Player {
+	private String name;
+
 	private List<Coordinate> targetForShot = new ArrayList<Coordinate>();
 	private List<Coordinate> shotHistory = new ArrayList<Coordinate>();
-	
-	
-	Player() {
-         	 for (int i = 1; i < 10; i++){	// we don't need the coordinate with 0 and 10 		 
-			   for (int j = 1; j < 10; j++){		
-					targetForShot.add(new Coordinate(i,j));
-			   }
-     		 }
-   	}
-         	
+
+	public Player(String name) {
+		this.name = name;
+		for (int i = 1; i < 10; i++) { // we don't need the coordinate with 0
+										// and 10
+			for (int j = 1; j < 10; j++) {
+				targetForShot.add(new Coordinate(i, j));
+			}
+		}
+	}
+
 	public Coordinate pli() {
 		Coordinate c;
 		int plicoordind;
 		Random coordind = new Random();
-		  plicoordind = coordind.nextInt(targetForShot.size());
-		  c = targetForShot.get(plicoordind);
-		  shotHistory.add(c);
-		  targetForShot.remove(c);
-//		  int enter = System.in.read();
-		  return c;
-    }
-	
+		plicoordind = coordind.nextInt(targetForShot.size());
+		c = targetForShot.get(plicoordind);
+		shotHistory.add(c);
+		targetForShot.remove(c);
+		// int enter = System.in.read();
+		return c;
+	}
+
 	public List<Coordinate> showShotHistory() {
 		return shotHistory;
 	}
-	
+
 	public int sizeOfTargetForShot() {
 		return targetForShot.size();
 	}
-	
+
 	public int madeShot() {
 		return (81 - targetForShot.size());
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
